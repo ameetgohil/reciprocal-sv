4817652a37ea135d3ba9537bc101ef17b4fb5069
qubits <ameet@siglabs.com>
2020-05-24 13:01:23 -0700

    add fixed2float for comparison, debug

--- sim/test/basic.js
+++ sim/test/basic.js
@@ -3,6 +3,7 @@ const {Sim, SimUtils, RisingEdge, RisingEdges, FallingEdge, FallingEdges, Edge, 
 const { Clock, Tick } = SimUtils;
 const {Elastic} = Interfaces;
 const _ = require('lodash');
+const f2f = require('fixed2float');
 
 const jsc = require('jsverify');
 const assert = require('assert');
@@ -37,7 +38,8 @@ describe('Basic Group', () => {
     this.timeout(10000); // Set timeout to expected run time of the test in ms
       setup('reciprocal_test');
       sim.addTask(function *() {
-          for(let i of _.range(0xfff)) {
+          for(let i of _.range(0xf)) {
+              console.log((f2f.toFixed(1.0/f2f.toFloat(i,6,10))).toString(16));
               dut.i_data(i);
               yield* Tick();
           }
